385ad9a52c418f34b6f847bc88b9abf89537ac56
Paulo Pitte <pauloanalista@outlook.com>
2025-03-10 17:15:25 -0300

    Adicionar arquivos de projeto.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Pitte.SOLID
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
